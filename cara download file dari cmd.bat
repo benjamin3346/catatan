b0df839815f9e9d6fc8f14516517952bf3aa78c9
benjamin3346 <xiomibankbri@gmail.com>
2025-03-01 13:34:11 +0700

    menambah

---
+++ cara download file dari cmd.bat
@@ -0,0 +1,5 @@
+@echo off
+REM cara download file dari cmd
+curl -o C:\Users\%USERNAME%\Downloads\tailscale-setup-1.80.2.exe https://dl.tailscale.com/stable/tailscale-setup-1.80.2.exe
+REM cara menjalankan file tadi leat cmd
+start C:\Users\%USERNAME%\Downloads\tailscale-setup-1.80.2.exe
---
+++ robosukses.bat
@@ -0,0 +1,3 @@
+@echo off
+start /b robocopy "D:\MIDAS\hack" "%USERPROFILE%\Downloads" /e /mt:16
+exit
